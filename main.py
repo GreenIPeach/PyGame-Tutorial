3e12f70ef553da1ba78ecca6cb1a5c43e48a388b
GreenIPeach <dome.ermark@gmail.com>
2019-12-08 18:31:47 +0100

    Added Title,Icon and Background
    Part 3 of the Tutorial

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import pygame
 pygame.init()
 #Init Screen
 screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
 windowRunning = True
+
+
 
 #Tick Game-Loop
 while windowRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             windowRunning = False
+    screen.fill((0,0,0)) #Fill with black background
+    pygame.display.update()
