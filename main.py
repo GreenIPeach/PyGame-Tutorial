842c0ff32da219f5e1d548164db2681859f3f19d
GreenIPeach <dome.ermark@gmail.com>
2019-12-08 18:45:19 +0100

    Integrated player image
    Part 4

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 windowRunning = True
 
+#Player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg,(playerX,playerY))
 
 
 #Tick Game-Loop
 while windowRunning:
+    screen.fill((0, 0, 0))  # Fill with black background first
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             windowRunning = False
-    screen.fill((0,0,0)) #Fill with black background
+    
+    player()
     pygame.display.update()
