4711345687b5c161a2b7083b7eea2754fce35478
GreenIPeach <dome.ermark@gmail.com>
2019-12-11 14:27:53 +0100

    Simple bullet shooting
    Part 11

--- main.py
+++ main.py
@@ -24,6 +24,16 @@ enemyY = random.randint(50, 150)
 enemyX_change = 2
 enemyY_change = 40
 
+# Bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+# Ready - You can´t see the bullet on the screen
+# Fire - The bullet is currently moving
+bullet_state = "ready"
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -31,6 +41,12 @@ def player(x, y):
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 
 # Tick Game-Loop
@@ -47,25 +63,32 @@ while windowRunning:
                 playerX_change = -3
             if event.key == pygame.K_RIGHT:
                 playerX_change = 3
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Player movement
     playerX += playerX_change
-
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
 
+    # Enemy movement
     enemyX += enemyX_change
-
     if enemyX <= 0:
         enemyX_change = 2
         enemyY += enemyY_change
     elif enemyX >= 736:
         enemyX_change = -2
         enemyY += enemyY_change
+
+    # Bullet movement
+    if bullet_state == "fire":
+        fire_bullet(playerX,bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
